57445bf7e61c7ec36ef9d95cb5933bc4d10c6b79
naithen018 <naithen018@gmail.com>
2026-06-04 13:41:44 +0530

    Added multiple file upload and file validations

--- task2/api/routes.py
+++ task2/api/routes.py
@@ -1,7 +1,36 @@
+from typing import List
 from fastapi import APIRouter, UploadFile, File
 
 router = APIRouter()
 
-@router.post("/single")
-async def single_file(file: UploadFile = File(...)):
-    return {"name": file.filename}
+ALLOWED_EXTENSIONS = {".pdf", ".png", ".jpg", ".jpeg"}
+
+
+@router.get("/")
+def home():
+    return {"message": "API is running"}
+
+
+@router.post("/upload")
+async def upload_files(files: List[UploadFile] = File(...)):
+
+    if len(files) > 3:
+        return {
+            "status": "error",
+            "message": "Maximum 3 files allowed"
+        }
+
+    for file in files:
+
+        filename = file.filename.lower()
+
+        if not filename.endswith(tuple(ALLOWED_EXTENSIONS)):
+            return {
+                "status": "error",
+                "message": f"Unsupported file type: {file.filename}"
+            }
+
+    return {
+        "status": "success",
+        "files": [file.filename for file in files]
+    }
