368efc4547f449d08f948c8bb3b0f0daa138f930
naithen018 <naithen018@gmail.com>
2026-06-07 23:48:19 +0530

    Added upload directory

--- task2/api/routes.py
+++ task2/api/routes.py
@@ -1,9 +1,18 @@
 from typing import List
 from fastapi import APIRouter, UploadFile, File
+from pathlib import Path
+from pypdf import PdfReader
+from io import BytesIO
+from config.settings import (
+    ALLOWED_EXTENSIONS,
+    MAX_FILES,
+    MAX_PDF_PAGES
+)
 
 router = APIRouter()
 
-ALLOWED_EXTENSIONS = {".pdf", ".png", ".jpg", ".jpeg"}
+UPLOAD_DIR = Path("storage/uploads")
+UPLOAD_DIR.mkdir(parents=True, exist_ok=True)
 
 
 @router.get("/")
@@ -14,7 +23,7 @@ def home():
 @router.post("/upload")
 async def upload_files(files: List[UploadFile] = File(...)):
 
-    if len(files) > 3:
+    if len(files) > MAX_FILES:
         return {
             "status": "error",
             "message": "Maximum 3 files allowed"
@@ -24,13 +33,36 @@ async def upload_files(files: List[UploadFile] = File(...)):
 
         filename = file.filename.lower()
 
+        # Check file extension
         if not filename.endswith(tuple(ALLOWED_EXTENSIONS)):
             return {
                 "status": "error",
                 "message": f"Unsupported file type: {file.filename}"
             }
 
+        # Read file contents
+        content = await file.read()
+
+        # PDF page validation
+        if filename.endswith(".pdf"):
+
+            pdf_reader = PdfReader(BytesIO(content))
+            page_count = len(pdf_reader.pages)
+
+            if page_count > MAX_PDF_PAGES:
+                return {
+                    "status": "error",
+                    "message": f"{file.filename} exceeds {MAX_PDF_PAGES} pages"
+                }
+
+        # Save file
+        file_path = UPLOAD_DIR / file.filename
+
+        with open(file_path, "wb") as buffer:
+            buffer.write(content)
+
     return {
-        "status": "success",
-        "files": [file.filename for file in files]
-    }
+    "status": "success",
+    "message": "Files uploaded successfully",
+    "files": [file.filename for file in files]
+}
---
+++ task2/config/settings.py
@@ -0,0 +1,5 @@
+ALLOWED_EXTENSIONS = {".pdf", ".png", ".jpg", ".jpeg"}
+
+MAX_FILES = 3
+
+MAX_PDF_PAGES = 5
