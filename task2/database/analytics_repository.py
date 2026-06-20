8f91921defa314a9375a16e265dead0ecb755d45
Jewel <116877680+Jewelmj@users.noreply.github.com>
2026-06-20 23:53:32 +0530

    feat: add analytics repository

---
+++ task2/database/analytics_repository.py
@@ -0,0 +1,44 @@
+from database.connection import db
+
+class AnalyticsRepository:
+    def __init__(self):
+        self.collection = (
+            db.analytics
+        )
+
+    def save(self,record: dict):
+        result = (
+            self.collection.insert_one(
+                record
+            )
+        )
+
+        return str(result.inserted_id)
+
+    def get_by_id(self,interaction_id: str):
+
+        return (
+            self.collection.find_one(
+                {
+                    "interaction_id":
+                    interaction_id
+                }
+            )
+        )
+
+    def update_feedback(self,interaction_id: str,feedback: str):
+
+        return (
+            self.collection.update_one(
+                {
+                    "interaction_id":
+                    interaction_id
+                },
+                {
+                    "$set": {
+                        "feedback":
+                        feedback
+                    }
+                }
+            )
+        )
