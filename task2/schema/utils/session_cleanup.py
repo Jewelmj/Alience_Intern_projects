bb61c2b9e7f9bcb465e0113242aa3de28767de50
Jewel <116877680+Jewelmj@users.noreply.github.com>
2026-07-01 16:05:51 +0530

    Refactor: cleanup logic into shared helper

--- task2/schema/utils/session_cleanup.py
+++ task2/schema/utils/session_cleanup.py
@@ -1,76 +1,14 @@
-from pathlib import Path
 from datetime import datetime, timedelta, UTC
 
 from config.settings import (
-    FILE_RETENTION_DAYS,
     SESSION_TIMEOUT_MINUTES
 )
 
 from database.document_repository import (
-    get_old_documents,
-    delete_document,
     get_expired_sessions
 )
 
-from database.vector_repository import (
-    delete_document_embeddings
-)
-
-from config.logger import logger
-
-def delete_document_resources(document):
-    filename = document["filename"]
-
-    delete_document_embeddings(
-        filename
-    )
-
-    files_to_delete = [
-
-        Path("storage/uploads")
-        / filename,
-
-        Path(
-            "storage/extracted_text"
-        )
-        / document.get(
-            "text_file",
-            ""
-        ),
-
-        Path(
-            "storage/chunks"
-        )
-        / document.get(
-            "chunk_metadata_file",
-            ""
-        ),
-
-        Path(
-            "storage/vectors"
-        )
-        / document.get(
-            "vector_file",
-            ""
-        )
-    ]
-
-    for file_path in files_to_delete:
-
-        if file_path.name == ".gitkeep":
-            continue
-
-        if file_path.exists():
-
-            file_path.unlink()
-
-            logger.info(
-                f"Deleted {file_path}"
-            )
-
-    delete_document(
-        str(document["_id"])
-    )
+from schema.utils.document_cleanup import delete_document_resources
 
 def cleanup_expired_sessions():
     cutoff = (
